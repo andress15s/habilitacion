8c29066d2a5798d434011ddc8e12cdf3635b1a1d
andress15s <carlos2214104@correo.uis.edu.co>
2022-12-06 15:09:25 -0500

    ayuda

---
+++ modelo/libreta.java
@@ -0,0 +1,51 @@
+package modelo;
+
+public class libreta 
+{
+    // atributos 
+    public int n ;
+    public String O;
+    public String A;
+    public String x;
+
+    
+String [] libreta = new libreta()
+{
+    if (n >= 10 & n <= 50)
+    {
+        System.out.println("el numero de hojas es: " + n );
+    }
+
+    else ()
+    {
+        System.out.println("el numero de hojas es: 50");
+    }
+}
+
+
+public class libreta
+
+{
+    
+        // 
+        private String libreta ;
+        ;
+        
+        public libreta(int n, String x,String O,String A ) 
+        {
+            numerohojas = n;
+            estadohojas = O;
+        } 
+    
+        public void Agregarhojas(int cantidad)
+         {
+           n = n + cantidad;
+        } 
+    
+        public void retirar(int cantidad) {
+            saldo = saldo - cantidad;
+        } 
+    
+        public int saldo() {
+            return saldo;
+        }   
